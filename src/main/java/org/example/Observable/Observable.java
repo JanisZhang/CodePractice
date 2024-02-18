fc9c8f5faf7df533db30bb881582836430c5c13b
JanisZhang <janis.zhang@liferay.com>
2024-02-18 18:16:38 +0800

    Single Thread execution

---
+++ src/main/java/org/example/Observable/Observable.java
@@ -0,0 +1,12 @@
+package org.example.Observable;
+
+public interface Observable {
+    enum Cycle{
+        STARTED, RUNNING, DONE, ERROR
+    }
+
+    Cycle getCycle();
+
+    void start();
+    void interrupt();
+}
---
+++ src/main/java/org/example/Observable/TaskLifestyle.java
@@ -0,0 +1,9 @@
+package org.example.Observable;
+
+public interface TaskLifestyle<T> {
+
+    void onStart(Thread thread);
+    void onRunning(Thread thread);
+    void onFinish(Thread thread, T result);
+    void onError(Thread thread, Exception e);
+}
